d7c05f49ca3597af777818d3b257ff836782c820
iDontHaveTime <cheesedeveloperworking@gmail.com>
2026-02-23 20:57:16 +0200

    [docs]: Token header documentation more or less finished.

--- src/token/token.hpp
+++ src/token/token.hpp
@@ -1,67 +1,70 @@
 #pragma once
 #include <string>
 
+///
+/// @brief Determines the type the token will have.
+///
 enum TokenType {
 
-  IMPORT = 1,
-  FUN = 2,
-  RETURN = 3,
-  IF = 4,
-  ELSE = 5,
-  WHILE = 6,
-  FOR = 7,
-  MATCH = 8,
-  VAR = 9,
-  EXTERN = 10,
-  MODULE = 11,
-  PUB = 12,
-  PRIV = 13,
-  RECORD = 14,
-  IMPL = 15,
-  STATIC = 16,
-  ENUM = 17,
-  SEMICOLON = 18,
-  COLON = 19,
-  DOUBLECOLON = 20,
-  DOT = 21,
-  COMMA = 22,
-  LPAREN = 23,
-  RPAREN = 24,
-  LBRACE = 25,
-  RBRACE = 26,
-  ARRAY = 27,
-  SQUARE_LBRACE = 28,
-  SQUARE_RBRACE = 29,
-  QUESTION = 30,
-  ARROW = 31,
-  LAMBDA = 32,
-  LESSEQUAL = 33,
-  LESS = 34,
-  GREATER = 35,
-  GREATEREQUAL = 36,
-  EQUAL = 37,
-  ASSIGN = 38,
-  PLUS = 39,
-  MINUS = 40,
-  MULTIPLY = 41,
-  REFERENCE = 42,
-  DIVIDE = 43,
-  MODULO = 44,
-  POW = 45,
-  NOT = 46,
-  AND = 47,
-  OR = 48,
-  ID = 49,
-  INTEGER = 50,
-  FLOAT = 51,
-  STRING = 52,
-  CHAR = 53,
-  BOOL = 54,
-  BREAK = 55,
-  CONTINUE = 56,
-  ELLIPSIS = 57, // ...
-  NOTEQUAL = 58,
-  CONCAT = 59, // ~
+  IMPORT = 1, ///< "import" keyword.
+  FUN = 2, ///< "fun" (Function) keyword.
+  RETURN = 3, ///< "return" or "ret" keyword.
+  IF = 4, ///< "if" keyword.
+  ELSE = 5, ///< "else" keyword.
+  WHILE = 6, ///< "while" keyword.
+  FOR = 7, ///< "for" keyword.
+  MATCH = 8, ///< "match" keyword.
+  VAR = 9, ///< "var" keyword.
+  EXTERN = 10, ///< "ext" (Extern) keyword.
+  MODULE = 11, ///< "module" keyword.
+  PUB = 12, ///< "pub" (Public) keyword.
+  PRIV = 13, ///< "priv" (Private) keyword.
+  RECORD = 14, ///< "record" keyword.
+  IMPL = 15, ///< "impl" keyword.
+  STATIC = 16, ///< "static" keyword.
+  ENUM = 17, ///< "enum" keyword.
+  SEMICOLON = 18, ///< ';' symbol.
+  COLON = 19, ///< ':' symbol.
+  DOUBLECOLON = 20, ///< '::' symbol.
+  DOT = 21, ///< '.' symbol.
+  COMMA = 22, ///< ',' symbol.
+  LPAREN = 23, ///< '(' symbol.
+  RPAREN = 24, ///< ')' symbol.
+  LBRACE = 25, ///< '{' symbol.
+  RBRACE = 26, ///< '}' symbol.
+  ARRAY = 27, ///< ???
+  SQUARE_LBRACE = 28, ///< '[' symbol.
+  SQUARE_RBRACE = 29, ///< ']' symbol.
+  QUESTION = 30, ///< ';' symbol.
+  ARROW = 31, ///< '->' symbol.
+  LAMBDA = 32, ///< ? '=>' symbol ?
+  LESSEQUAL = 33, ///< '<=' symbol.
+  LESS = 34, ///< '<' symbol.
+  GREATER = 35, ///< '>' symbol.
+  GREATEREQUAL = 36, ///< '>=' symbol.
+  EQUAL = 37, ///< '==' symbol.
+  ASSIGN = 38, ///< '=' symbol.
+  PLUS = 39, ///< '+' symbol.
+  MINUS = 40, ///< '-' symbol.
+  MULTIPLY = 41, ///< '*' symbol.
+  REFERENCE = 42, ///< '&' symbol.
+  DIVIDE = 43, ///< '/' symbol.
+  MODULO = 44, ///< '%' symbol.
+  POW = 45, ///< '^' symbol.
+  NOT = 46, ///< '!' symbol.
+  AND = 47, ///< '&&' symbol.
+  OR = 48, ///< '||' symbol.
+  ID = 49, ///< Identifier.
+  INTEGER = 50, ///< Integer.
+  FLOAT = 51, ///< Floating point.
+  STRING = 52, ///< String.
+  CHAR = 53, ///< Char.
+  BOOL = 54, ///< True or false.
+  BREAK = 55, ///< "break" keyword.
+  CONTINUE = 56, ///< "continue" keyword.
+  ELLIPSIS = 57, ///< '...' symbol.
+  NOTEQUAL = 58, ///< '!=' symbol.
+  CONCAT = 59, ///< '~' symbol.
   VAL,
 };
 
@@ -69,14 +72,27 @@ enum TokenType {
 /// @brief Contains in-file related information like line, column, offset, and length.
 ///
 struct SourceSpan {
-  size_t line;
-  size_t column;
-  size_t offset;
-  size_t length;
+  size_t line; ///< Line of the source in the file.
+  size_t column; ///< Column of the source in the file.
+  size_t offset; ///< Offset of the source in the file.
+  size_t length; ///< Length of the source.
 
+  ///
+  /// @brief Basic constructor of the source span.
+  /// @param l Line.
+  /// @param c Column.
+  /// @param o Offset.
+  /// @param len Length.
+  ///
   SourceSpan(size_t l = 0, size_t c = 0, size_t o = 0, size_t len = 0)
       : line(l), column(c), offset(o), length(len) {}
 
+  ///
+  /// @brief Merges two 'SourceSpan' classes.
+  /// @param start From.
+  /// @param end To.
+  /// @return Merged 'SourceSpan'.
+  ///
   static SourceSpan merge(const SourceSpan &start, const SourceSpan &end) {
     size_t newLen = (end.offset + end.length) - start.offset;
     return SourceSpan(start.line, start.column, start.offset, newLen);
@@ -85,14 +101,19 @@ struct SourceSpan {
 
 class Token {
 public:
-  SourceSpan span;
-  TokenType type;
-  std::string value;
+  SourceSpan span; ///< Source of the token in the file.
+  TokenType type; ///< Type of the token.
+  std::string value; ///< String of the token.
 
+  ///
+  /// @brief Default constructor of the 'Token' class.
+  ///
   Token(TokenType type, const std::string &value, SourceSpan span)
       : span(span), type(type), value(value) {}
 
-  // Helper constructor for when we build span component-wise
+  ///
+  /// @brief Helper constructor for when we build span component-wise.
+  ///
   Token(TokenType type, const std::string &value, size_t line, size_t column,
         size_t offset, size_t length)
       : span(line, column, offset, length), type(type), value(value) {}
